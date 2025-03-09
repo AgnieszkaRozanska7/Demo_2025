cfd136f160a2fca52110319f347370f0caae8ff7
Agnieszka Różańska <arozanska124@gmail.com>
2025-03-09 13:19:07 +0100

    Wyczyszczenie kodu

--- CalculatorApp/Services/Calculate.cs
+++ CalculatorApp/Services/Calculate.cs
@@ -1,9 +1,4 @@
 ﻿using CalculatorApp.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculatorApp.Services
 {
--- CalculatorApp/Services/Interfaces/ICalculate.cs
+++ CalculatorApp/Services/Interfaces/ICalculate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculatorApp.Services.Interfaces
+﻿namespace CalculatorApp.Services.Interfaces
 {
     internal interface ICalculate
     {
